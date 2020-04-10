a65fcf2080951495d0c02919a484adb0cf543777
Griffin-MX <1123306966@qq.com>
2020-04-10 22:00:02 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 var config = {
 
     texts: [
-        "送给我",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
-        "Boss",
+        "生日祝福送给你",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "老大",
         "今天是你的生日",
-        "YAMI~~",
+        "愿你每天都能拥有好心情",
+        "祝早日暴富",
     ],
     /**
      * "对应上面的文字, 要完全一样" : "图片地址, 可以把图片放在imgs文件夹中"
@@ -14,17 +15,17 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "Boss": "./imgs/bosshead.jpg",
+        "老大": "./bosshead.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
         turn_on: "开启2020.4.11",
         play: "来点音乐？",
-        bannar_coming: "颜色",
+        bannar_coming: "拉个横幅",
         balloons_flying: "好像少点东西",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
         wish_message: "生日快乐",
-        story: "A MESSAGE FOR YOU",
+        story: "请查收来自小弟的祝福",
     }
 };
